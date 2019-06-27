b1d54328afc2ecb8b443089d3996dc3d2a1b23c6
tan102 <398869187@qq.com>
2019-06-27 09:03:32 +0800

    FontAweSomeTest

---
+++ FontAweSomeTest/FontAweSomeTest/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FontAweSomeTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnWrapPanelLoaded(object sender, RoutedEventArgs e)
+        {
+            var wrap = (WrapPanel)sender;
+
+            var dic = this.Resources.MergedDictionaries.First();
+
+            foreach(var key in dic.Keys)
+            {
+                wrap.Children.Add(new TextBlock() { Text = dic[key].ToString() });
+            }
+
+        }
+    }
+}
